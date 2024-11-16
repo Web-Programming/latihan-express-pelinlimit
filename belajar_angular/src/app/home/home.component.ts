fe4138b0145d46eb9dd275332d265b5aa733087e
pelinlimited <felinnn96@gmail.com>
2024-11-16 17:00:03 +0700

    commit

---
+++ belajar_angular/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  HousingService: HousingService = inject(HousingService);
+
+  constructor(){
+    this.housingLocationList = this.HousingService.getAllHousingLocations();
+  }
+}
+
---
+++ belajar_angular/src/app/housing.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  protected housingLocationList: HousingLocation[] = [
+    {
+      id : 0,
+      name : "Citra Grand City",
+      city : "Palembang",
+      state : "indonesia",
+      photo : "https://static-id.lamudi.com/static/media/bm9uZS9ub25l/1000x620/8d2d212e30667b.jpg",
+      availableUnits : 12,
+      wifi : true,
+      laundry : true,
+    },
+    {
+      id : 1,
+      name : "Citra Land",
+      city : "Palembang",
+      state : "indonesia",
+      photo : "https://incomerealty.id/wp-content/uploads/2023/12/DIJUAL-RUMAH-TROPICAL-VALLEY-CGC-PALEMBANG-1.jpg",
+      availableUnits : 14,
+      wifi : true,
+      laundry : true,
+    }
+    
+  ]
+
+  constructor() { }
+  getAllHousingLocations() : HousingLocation[]{
+    return this.housingLocationList;
+  }
+  getHousingLocationById(id : Number) : HousingLocation | undefined { 
+    return this.housingLocationList.find(housingLocation => housingLocation.id == id);
+  }
+}
